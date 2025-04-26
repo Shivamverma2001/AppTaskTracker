9ff7ff937497eefd107addb1e3f5e6db386f6b4f
Shivam verma <shivamverma2001sj@gmail.com>
2025-04-26 12:56:56 +0530

    fix the build

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,26 +1,29 @@
 import express from 'express';
 import cors from 'cors';
-const app = express();
-const port = process.env.PORT || 3000;
 import connectDB from './db.js';
 import router from './routes.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const port = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
-
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
-
-app.get('/api/v1/users', (req, res) => {
-    res.send('Hello World');
-});
+if (process.env.NODE_ENV === 'production') {
+    const staticPath = path.join(__dirname, '..', 'AppTaskTraker', 'dist');
+    app.use(express.static(staticPath));
+    console.log('Production mode');
+    console.log('Serving static files from:', staticPath);
+    
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(staticPath, 'index.html'));
+    });
+}
 
 connectDB().then(() => {
     app.listen(port, () => {
